ce8ebf32ab470008a2a715f5649a08508e711040
Yousef Ayman <yousefayman20003@gmail.com>
2024-04-23 14:05:19 -0700

    update task 5,6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+export default async function handleProfileSignup(firstName, lastName, filename) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((r) => (
-      r.map((obj) => (
-        { status: obj.status, value: obj.status === 'fulfilled' ? obj.value : String(obj.reason) }
-      ))
+      r.map((obj) => ({
+        status: obj.status,
+        value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
+      }))
     ));
 }
